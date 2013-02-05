b6c45c19394a4176d2e79909ddb4a24743016253
Kevin van der Vleuten <kevin.vd.vleuten@gmail.com>
2013-02-05 20:39:09 +0100

    created a new class

---
+++ src/week2/TrianglePrinter.java
@@ -0,0 +1,24 @@
+package week2;
+
+public class TrianglePrinter {
+	static int side1;
+	static int side2;
+	static int side3;
+	
+	public TrianglePrinter(int side1, int side2, int side3)
+	{
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+	
+	public static int Perimeter()
+	{
+		return side1 + side2 + side3;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Perimeter is: " + Perimeter());
+	}
+}
